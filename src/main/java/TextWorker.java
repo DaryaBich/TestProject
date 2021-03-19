415d4571165bd5c68b7fa2e884816f2a4e04f540
DaryaBich <62696076+DaryaBich@users.noreply.github.com>
2021-03-19 16:06:33 +0400

    Update TextWorker.java

--- src/main/java/TextWorker.java
+++ src/main/java/TextWorker.java
@@ -3,20 +3,22 @@ import java.util.LinkedList;
 import java.util.Map;
 
 public final class TextWorker {
-    private static final String regexp = "\\s|,|!|\\.|\"|;|\\?|:|\\]|\\[|}|\\{|\n|\t|\r|»|«|\\d|%|<|>|\\(|\\)|©|/";
+    private static final String SPLITTER = "\\s|,|!|\\.|\"|;|\\?|:|\\]|\\[|}|\\{|\n|\t|\r|»|«|\\d|%|<|>|\\(|\\)|©|/";
+    
+    private TextWorker(){}
 
     public static void splitAndPrintText(LinkedList<String> stripesAccumulatorText) {
-        HashMap<String, Integer> wordsCount = TextWorker.splitText(stripesAccumulatorText);
-        TextWorker.sortAndPrintText(wordsCount);
+        HashMap<String, Integer> wordsCounter = splitText(stripesAccumulatorText);
+        sortAndPrintText(wordsCounter);
     }
 
     private static HashMap<String, Integer> splitText(LinkedList<String> stripes) {
         HashMap<String, Integer> countOfWords = new HashMap<>();
         for (String stripe : stripes) {
-            String[] words = stripe.split(regexp);
+            String[] words = stripe.split(SPLITTER);
             for (String word : words) {
                 word = word.toLowerCase().trim();
-                if (!(word.equals("") || word.equals("—") || word.equals("-") || word.equals("–") ||
+                if (!(word.equals("") || word.equals("—") || word.equals("-") || word.equals("–") || //это муть какая-то
                         word.startsWith("-"))) {
                     int count = countOfWords.containsKey(word) ? countOfWords.get(word) + 1 : 1;
                     countOfWords.put(word, count);
